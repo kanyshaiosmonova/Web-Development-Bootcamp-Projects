83efcf23957382ce1e3d91bb35677d39209ec3ce
Kanyshai Osmonova <osmonova.kanyshai@gmail.com>
2022-03-24 19:03:27 +0600

    life in weeks challenge

---
+++ Exercise/lifeInWeeks.js
@@ -0,0 +1,17 @@
+function lifeInWeeks(age) {
+    
+    /************Don't change the code above************/    
+        
+        //Write your code here.
+    var yearsRemaining = 90 - age;
+    var months = yearsRemaining  *12;
+    var days = yearsRemaining * 365;
+    var weeks = yearsRemaining * 52;
+        
+    console.log("You have " + days +" days, "+ weeks + " weeks, and "+ months+" months left.");
+        
+        
+    /*************Don't change the code below**********/
+    }
+    
+    
