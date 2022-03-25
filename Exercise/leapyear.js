44c5c1ae9ec6c6c7f901b4f3e5884efea569c5f3
Kanyshai Osmonova <osmonova.kanyshai@gmail.com>
2022-03-26 03:28:11 +0600

    add js project

---
+++ Exercise/leapyear.js
@@ -0,0 +1,12 @@
+// Leap Year
+function isLeap(year) {
+    if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+
+    return "Leap year.";
+
+   } else {
+
+    return "Not leap year.";
+
+}
+}
