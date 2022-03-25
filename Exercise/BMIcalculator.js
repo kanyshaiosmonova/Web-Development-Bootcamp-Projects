58cc898c7f42ec267bbda53a7926e314598f12a6
Kanyshai Osmonova <osmonova.kanyshai@gmail.com>
2022-03-26 03:13:53 +0600

    if/else conditions

--- Exercise/BMIcalculator.js
+++ Exercise/BMIcalculator.js
@@ -6,3 +6,21 @@ function bmiCalculator(weight, height) {
     return Math.round(bmi);
 }
 console.log(57, 1.55);
+
+
+// if/else
+function bmiCalculator (weight, height) {
+    
+    var bmi = weight / Math.pow(height, 2);
+    var interpretation = "";
+    
+    if (bmi < 18.5) {
+        interpretation = "Your BMI is " + bmi + ", so you are underweight." ;
+    } else if (bmi < 24.9) {
+        interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+    } else {
+        interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+    }
+    
+    return interpretation;
+}
